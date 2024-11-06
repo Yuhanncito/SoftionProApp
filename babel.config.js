2536a701f4a8fda468621f93022b1ff6ee914204
GerardoOli <gera73.372@gmail.com>
2024-11-06 04:10:05 -0600

    feat : Pruebas archivo jest.config | Gerardo

--- babel.config.js
+++ babel.config.js
@@ -4,14 +4,6 @@ module.exports = function (api) {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-      [
-        "@babel/preset-env",
-        {
-          targets: {
-            node: "current",
-          },
-        },
-      ],
     ],
   };
 };
